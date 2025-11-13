1ebaa5af25f2b6bf40c218deca735bcbaec45b0b
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-11-13 13:37:30 +0100

    Esercizio3_1.0

---
+++ Sistemi_operativi/MultiProcC/lab01/Esercizio3.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+// Dimensione massima del nome del file
+#define DIM 50
+
+int main(int argc, char *argv[]){
+  int pid, status, fd, num;
+  char nome[DIM];
+
+  // Controllo il numero degli argomenti
+  if(argc != 2){
+    printf("Uso: ./anteprima <num>\n");
+    exit(1);
+  }
+
+  /* atoi non rileva errori di conversione,
+     meglio usare strtol() / strtoul() */
+  if( (num = atoi(argv[1])) <= 0 ){
+    printf("Errore: %s deve essere un intero positivo maggiore di zero\n", argv[1]);
+    exit(2);
+  }
+
+  printf("Inserisci il nome del file di cui visualizzare l'anteprima (\"fine\" per uscire): ");
+  scanf("%s", nome);
+
+  while(strcmp(nome,"fine") != 0){
+    // Per controllare se il file esiste, provo ad aprirlo
+    if((fd = open(nome, O_RDONLY)) < 0){
+      /* La open potrebbe fallire anche se il file esiste,
+         ad esempio nel caso manchino i permessi di lettura.
+         Per gestire la differenza, possiamo guardare il valore di errno.
+      */
+      if(errno == ENOENT){
+        printf("Il file %s non esiste\n", nome);
+      }else{
+        // Errore generico
+        perror("open");
+        exit(3);
+      }
+    }else{
+      /* Chiudo il file, che a questo punto esiste.
+         Lasciandolo aperto, causeremmo un resource
+         leak, che va evitato.
+      */
+      close(fd);
+      // Creo un figlio per visualizzare l'anteprima
+      pid = fork();
+      if(pid < 0){
+        perror("fork");
+        exit(4);
+      }else if(pid == 0){
+        // Figlio
+        printf("\nAnteprima del file %s:\n", nome);
+        execlp("head","head","-n", argv[1], nome, (char *)0);
+
+        perror("exec");
+        exit(5);
+      }
+      /* Padre: aspetto che il figlio termini
+         --> esecuzione sequenziale!
+      */
+      wait(&status);
+    } // Fine if/else
+
+    printf("\nInserisci il nome del file di cui visualizzare l'anteprima (\"fine\" per uscire): ");
+    scanf("%s", nome);
+  }// Fine while
+
+  // Esco senza errori
+  return 0;
+}
