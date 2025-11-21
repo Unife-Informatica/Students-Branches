10fcaec7d7a439b876890c28b412ef8a1a02a3de
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-11-21 10:51:05 +0100

    div_1.0

---
+++ Linguaggi_programmazione/eserciziAPI/div/Main.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    IntStream numbers = new Random(0).ints().map(Math::abs);
+
+    List<Integer> risultato = div17(numbers);
+    System.out.println(risultato);
+  }
+  public static List<Integer> div17(IntStream numbers){
+    return numbers.filter(n -> n%17==0).limit(5).mapToObj(n -> n).collect(Collectors.toList());
+  }
+}
