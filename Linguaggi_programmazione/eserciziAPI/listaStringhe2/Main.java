082f979e06cee7847b7a5648575c9bb664257ddb
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-12-11 09:49:52 +0100

    ListaStringhe2_1.0

---
+++ Linguaggi_programmazione/eserciziAPI/listaStringhe2/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<String> nomi = Arrays.asList("Luna", "Stitch", "Miguel", "Dory", "Speedy");
+    nomi.forEach(System.out::println);
+  }
+}
