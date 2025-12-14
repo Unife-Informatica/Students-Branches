ef966a6c94740677efc4cfa27359ee088ba36a14
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-12-14 11:46:44 +0100

    Discoteca_Beta

--- Linguaggi_programmazione/esercizi/demo/src/main/java/com/example/Main.java
+++ Linguaggi_programmazione/esercizi/demo/src/main/java/com/example/Main.java
@@ -3,8 +3,6 @@ package com.example;
 import com.example.ui.ClientiView;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
