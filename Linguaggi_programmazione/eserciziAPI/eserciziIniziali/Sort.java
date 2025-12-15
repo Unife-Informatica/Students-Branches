944ee96e706d0a36b1e6de8df48dc245fc308b5c
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-12-15 10:00:55 +0100

    Sort_2.0

--- Linguaggi_programmazione/eserciziAPI/eserciziIniziali/Sort.java
+++ Linguaggi_programmazione/eserciziAPI/eserciziIniziali/Sort.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 public class Sort {
   public static void main(String[] args) {
-    List<Node> list = Arrays.asList(new Node("c"), new Node("b"), new Node("d"), new Node("a"));
+    List<Node> list = Arrays.asList(new Node("c"), new Node("b"), new Node ("d"), new Node("a"));
 
-    Collections.sort(list, (a,b) -> a.getName().compareTo(b.getName()));
-    System.out.println(list);
+    Collections.sort(list, (a,b)->a.getName().compareTo(b.getName()));
   }
 }
