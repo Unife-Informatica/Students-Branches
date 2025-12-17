f4b7bd62e7b378d8a8b04b646ba739df5f6ec6c9
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2025-12-17 09:49:35 +0100

    Stringhe_2.0

--- Linguaggi_programmazione/eserciziAPI/stringhe/Main.java
+++ Linguaggi_programmazione/eserciziAPI/stringhe/Main.java
@@ -3,6 +3,6 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     String array[] = {"Mela", "Pera", "Arancia", "Fragola", "Kiwi", "Arancia"};
-    Arrays.stream(array).distinct().forEach(s -> System.out.println());
+    Arrays.stream(array).distinct().forEach(s -> System.out.println(s));
   }
 }
