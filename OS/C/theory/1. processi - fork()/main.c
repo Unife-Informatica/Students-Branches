80a25372c4519b02322fe08cb851072cdbeaee66
pietro <pietro.peerani@gmail.com>
2026-01-29 18:05:22 +0100

    update: file comments

--- OS/C/theory/1. processi - fork()/main.c
+++ OS/C/theory/1. processi - fork()/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <threads.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 /*
 +------------------------------------+----------------------------------------------------------+
@@ -16,23 +18,40 @@
 +------------------------------------+----------------------------------------------------------+
 */
 
+/*
+ * Funzione fork()
+ * * processo padre  -> ritorna il pid del processo figlio (valore > 0)
+ * * processo figlio -> ritorna 0
+ * * errore          -> ritorna -1
+ */
+
 int main() {
   pid_t pid;
+  int a = 5;
 
   printf("Prima della fork pid = %d, pid del genitore = %d\n", getpid(), getppid());
   if ((pid = fork()) < 0) {
     // ERRORE -> solitamente se finisce la memoria
     printf("Errore durante la fork.\n");
-  } else if (ret == 0) {
+  } else if (pid == 0) {
     // CODICE FIGLIO
     printf("[Figlio]: valore restituito dalla fork: %d\n", pid);
     printf("[Figlio]: pid = %d, pid del genitore = %d\n", getpid(), getppid());
+
+    a = 10;
+    printf("a = %d", a); // Output = 10
   } else {
     // CODICE PADRE
     printf("[Genitore]: valore restituito dalla fork: %d\n", pid);
     printf("[Genitore]: pid = %d, pid del mio genitore = %d\n", getpid(), getppid());
     printf("[Genitore]: mio figlio ha pid = %d\n", pid);
+
+    printf("a = %d", a); // Output = 5
   }
+
+  // aspetta che il figlio finisca l'esecuzione
+  // se il padre finisce prima del figlio quest'ultimo avrà un parent id errato
+  wait(NULL);
 
   // CODICE CONDIVISO
   return 0;
