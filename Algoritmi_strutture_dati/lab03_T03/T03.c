7f8e54cbcd8d450a6f0c20c20324daa50aadedff
GabrieleLambertini <gabriele.lambertini@edu.unife.it>
2026-04-17 17:21:40 +0200

    T03_1.0

---
+++ Algoritmi_strutture_dati/lab03_T03/T03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pianeta_spritz(FILE *inFile, FILE *outFile){
+  int N, Q;
+  fscanf(inFile, "%d %d", &N, &Q);
+
+  // distanze di ogni (x,y,z) da (0,0,0)
+  long long int dist[N];
+
+  for(int i = 0; i < N; ++i){
+    long long int x, y, z;
+    fscanf(inFile, "%lld %lld %lld", &x, &y, &z);
+
+    dist[i] = x*x, y*y, z*z;
+  }
+}
+
+int main(int argc, char const *argv[]){
+  FILE *inFile = fopen("input.txt", "r");
+  FILE *outFile = fopen("output.txt" "w");
+
+  pianeta_spritz(inFile, outFile);
+
+  fclose(inFile);
+  fclose(outFile);
+
+  return EXIT_SUCCESS;
+}
